73fac5f194b8fc7bcf4d739a2663481ec735ff06
hanna <hannawkozak@gmail.com>
2025-03-10 22:22:20 +0000

    setup: install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import './App.css';
+import { Button } from '@/components/ui/button';
+
 function App() {
-  return <>Trip Planner</>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Trip Planner</Button>
+    </div>
+  );
 }
 
 export default App;
